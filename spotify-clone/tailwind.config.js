01dae97bff238180cbbe236e1f246d1a3a3547b5
i252-hub <jushirarey@gmail.com>
2025-03-18 12:57:29 +0800

    chore: re-installed dependencies for tailwind

---
+++ spotify-clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./main.html",  
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
