e3ea8dc09e370f792f382f19bce7946e1978f032
i252-hub <jushirarey@gmail.com>
2025-03-21 11:10:42 +0800

    style: grid layout and bg colors

---
+++ spotify-clone/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-white">
+        <div className="grid grid-cols-[20%_80%] h-full">
+          <div className="bg-[#000000] h-full">hi</div>
+          <div className="bg-gradient-to-r from-[#3333A3] to-[#121212] h-full">hello</div>
+        </div>
+      </div>
+       
+    </>
+  )
+}
+
+export default Home;
